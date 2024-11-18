022acc4f66caa9156640d557138e2cecd67fde76
ByteAtATime <byteatatime@proton.me>
2024-11-17 20:33:37 -0800

    stuff

--- src/pages/api/users/[userId]/index.ts
+++ src/pages/api/users/[userId]/index.ts
@@ -2,6 +2,8 @@ import type { APIRoute } from "astro";
 import { clerkClient } from "@clerk/astro/server";
 import { displayUser } from "@lib/users/utils";
 import { getUserExperience } from "@lib/firebase/xp.ts";
+import { getAllEvents } from "@lib/firebase/events.ts";
+import { userMetadataSchema } from "@lib/firebase/types.ts";
 
 // sending public data
 export const GET: APIRoute = async (context) => {
@@ -17,11 +19,20 @@ export const GET: APIRoute = async (context) => {
   if (!user) return new Response("User not found", { status: 404 });
 
   if (user.publicMetadata.isAdmin || userId === user.id) {
+    const allEvents = await getAllEvents();
+    const eventsAttended = userMetadataSchema.parse(user.publicMetadata).eventsAttended;
+
+    const attendedEvents = allEvents.reduce<Record<string, boolean>>((acc, event) => {
+      acc[event.id] = eventsAttended.includes(event.id);
+      return acc;
+    }, {});
+
     const response = {
       displayName: displayUser(user, false),
       avatar: user.imageUrl,
       xp: await getUserExperience(userId),
       prizes: user.publicMetadata.prizesRedeemed,
+      attendedEvents
     }
 
     return new Response(JSON.stringify(response), { status: 200 });
