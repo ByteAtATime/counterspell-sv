29e32851e9ac4d681af2a862b2e24c8eb57455d4
ByteAtATime <byteatatime@proton.me>
2024-11-24 08:11:44 -0800

    asdf

--- src/pages/api/hunts/index.ts
+++ src/pages/api/hunts/index.ts
@@ -1,5 +1,6 @@
 import type { APIRoute } from "astro";
 import { createHunt } from "@lib/firebase/hunt.ts";
+import { huntSchema } from "$lib/lib/firebase/types";
 
 export const POST: APIRoute = async (context) => {
   const user = await context.locals.currentUser();
@@ -8,7 +9,11 @@ export const POST: APIRoute = async (context) => {
   }
 
   const huntData = await context.request.json();
-  const huntId = await createHunt(huntData);
+  const parsedHuntData = huntSchema.omit({ id: true }).safeParse(huntData);
+  if (!parsedHuntData.success) {
+    return new Response(JSON.stringify(parsedHuntData.error), { status: 400 });
+  }
+  const huntId = await createHunt(parsedHuntData.data);
 
   return new Response(JSON.stringify({ id: huntId }), { status: 201 });
 };
